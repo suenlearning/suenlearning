caa1a4e5235e805f19fc4a9c9a24231fdaae95bd
Marcjanna Stopinska <marcjanna.stopinska@gmail.com>
2021-02-11 13:45:41 +0100

    style home container for mobile version

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: var(--colorMain);
-  width: 100%;
 }
 
 /* HEADER */
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,16 +51,17 @@ export default {
 .home__cards--container {
   height: 100%;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   text-align: center;
 }
 
 .home__cards--item {
-  margin: 0 15px;
-  padding: 20px 12px;
-  width: 154px;
-  border: 2px solid var(--colorBorder);
+  margin: 12px 0;
+  padding: 16px 8px;
+  width: 134px;
+  border: 1px solid var(--colorBorder);
   border-radius: 10px;
   /* height: 250px; */
 }
@@ -72,20 +73,49 @@ export default {
 .card__img {
   /* options: use only images of the same size and alpha background or set fixed height to the lowest of the images */
   /* width: 100%; */
-  max-height: 100px;
+  max-height: 70px;
 }
 
 .card__title {
+  font-size: 16px;
   margin: 10px 0;
   font-weight: 600;
 }
 
 .card__description {
   font-weight: 300;
+  font-size: 16px;
 }
 
 .card__link {
   text-decoration: none;
   color: var(--colorMain);
+}
+
+@media screen and (min-width: 768px) {
+  .home__cards--container {
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .home__cards--item {
+    margin: 0 15px;
+    padding: 20px 12px;
+    width: 154px;
+    border: 2px solid var(--colorBorder);
+  }
+
+  .card__img {
+    max-height: 100px;
+  }
+
+  .card__title {
+    font-size: 20px;
+  }
+
+  .card__description {
+    font-size: 18px;
+  }
 }
 </style>
